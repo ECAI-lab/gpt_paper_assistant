ee272d9d33fc03801a09e04af8de0099cbb67449
ZiningZhu <zining@cs.toronto.edu>
2024-06-19 10:37:21 -0600

    Add pricing for gpt-4o

--- filter_papers.py
+++ filter_papers.py
@@ -55,13 +55,15 @@ def filter_papers_by_hindex(all_authors, papers, config):
 
 
 def calc_price(model, usage):
+    if model == "gpt-4o":
+        return (0.005 * usage.prompt_tokens + 0.015 * usage.completion_tokens) / 1000.0
     if model == "gpt-4-1106-preview":
         return (0.01 * usage.prompt_tokens + 0.03 * usage.completion_tokens) / 1000.0
     if model == "gpt-4":
         return (0.03 * usage.prompt_tokens + 0.06 * usage.completion_tokens) / 1000.0
     if (model == "gpt-3.5-turbo") or (model == "gpt-3.5-turbo-1106"):
         return (0.0015 * usage.prompt_tokens + 0.002 * usage.completion_tokens) / 1000.0
-
+    
 
 @retry.retry(tries=3, delay=2)
 def call_chatgpt(full_prompt, openai_client, model, num_samples):
