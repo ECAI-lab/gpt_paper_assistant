e904746f8b12d1970a4d7950502d76e6b84c1341
ZiningZhu <zining@cs.toronto.edu>
2025-06-10 10:34:09 -0400

    Do not delete the arxiv API option codes; comment out instead

--- arxiv_scraper.py
+++ arxiv_scraper.py
@@ -134,6 +134,10 @@ def merge_paper_list(paper_list, api_paper_list):
 
 def get_papers_from_arxiv_rss_api(area: str, config: Optional[dict]) -> List[Paper]:
     paper_list, timestamp, last_id = get_papers_from_arxiv_rss(area, config)
+    # if timestamp is None:
+    #     return []
+    # api_paper_list = get_papers_from_arxiv_api(area, timestamp, last_id)
+    # merged_paper_list = merge_paper_list(paper_list, api_paper_list)
     return paper_list
 
 
